7eefdcc4d8e8a958cd6f1960e2616c91e958ab6a
Nazar Ushenok <tvepzzz.dev@gmail.com>
2023-06-13 17:23:45 +0300

    change title

--- containers/Hero.jsx
+++ containers/Hero.jsx
@@ -4,17 +4,15 @@ import { Button } from '../components'
 const Hero = () => {
 	return (
 		<section>
-			<div className="hero__container container grid">
-				<img src="/hero.png" alt="hero img" className="hero__img" />
-				<div className="hero__data">
-					<h1 className="hero__title">
-						вАУ Десерт - десерти просто ВАУ!
+			<div className='hero__container container grid'>
+				<img src='/hero.png' alt='hero img' className='hero__img' />
+				<div className='hero__data'>
+					<h1 className='hero__title'>
+						вАУ Десерт - десерти від Анастасії Ушенок!
 					</h1>
-					<p className="hero__description">
-						Магазин домашніх солодощів
-					</p>
-					<Link href="/catalog">
-						<Button text="В каталог" />
+					<p className='hero__description'>Магазин домашніх солодощів</p>
+					<Link href='/catalog'>
+						<Button text='В каталог' />
 					</Link>
 				</div>
 			</div>
